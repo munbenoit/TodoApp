851e5b448d9d4c426561a902b328f5979a953409
benoit munoz <mun.benoit@gmail.com>
2019-04-15 16:57:35 +0200

    Add Basic Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos'
+import Header from './components/layout/Header'
 
 class App extends Component {
   state = {
@@ -41,7 +42,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Header />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
