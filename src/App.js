d92ba2400d8178a94fed05f677a4d32534c08934
benoit munoz <mun.benoit@gmail.com>
2019-04-15 16:45:17 +0200

    Change state + delete todo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       {
         id: 2,
         title : "Start the React Tutorial",
-        completed : false
+        completed : true
       },
       {
         id: 3,
@@ -23,11 +23,26 @@ class App extends Component {
     ]
   }
 
+  markComplete = (id) =>{
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id===id){
+        todo.completed = ! todo.completed
+      }
+      return todo;
+    })
+   })
+  }
+
+  delTodo = (id) => {
+    this.setState({todos : [...this.state.todos.filter(todo => todo.id !== id)]
+  })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>App</h1>
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
   }
